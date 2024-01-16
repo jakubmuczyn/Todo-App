b71aa8c1d7eb65358b999f746e761993d41d4296
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-01-16 22:42:07 +0100

    Add some annotations and validator

--- src/main/java/pl/jakubmuczyn/model/Task.java
+++ src/main/java/pl/jakubmuczyn/model/Task.java
@@ -1,22 +1,26 @@
 package pl.jakubmuczyn.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
+    
+    Task() {
+    }
     
     public int getId() {
         return id;
     }
     
-    public void setId(int id) {
+    void setId(final int id) {
         this.id = id;
     }
     
@@ -24,7 +28,7 @@ public class Task {
         return description;
     }
     
-    public void setDescription(String description) {
+    void setDescription(final String description) {
         this.description = description;
     }
     
@@ -32,7 +36,7 @@ public class Task {
         return done;
     }
     
-    public void setDone(boolean done) {
+    void setDone(final boolean done) {
         this.done = done;
     }
 }
