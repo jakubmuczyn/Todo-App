a505f5f035ce36f14dc9ca24677d4e5dbb0472a0
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-01 23:45:55 +0100

    HTTP PUT mapping

--- src/main/java/pl/jakubmuczyn/controller/TaskController.java
+++ src/main/java/pl/jakubmuczyn/controller/TaskController.java
@@ -1,10 +1,13 @@
 package pl.jakubmuczyn.controller;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.jakubmuczyn.model.Task;
 import pl.jakubmuczyn.model.TaskRepository;
@@ -12,7 +15,7 @@ import pl.jakubmuczyn.model.TaskRepository;
 import java.util.List;
 
 @RestController
-class  TaskController {
+class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
     
@@ -30,5 +33,11 @@ class  TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+    
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody @Valid Task toUpdate) {
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }
