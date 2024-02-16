beff4e968a85a1e171ff06a2ddd16b247ad8a417
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-16 21:56:48 +0100

    @Scope

--- src/main/java/pl/jakubmuczyn/logic/TaskGroupService.java
+++ src/main/java/pl/jakubmuczyn/logic/TaskGroupService.java
@@ -2,7 +2,7 @@ package pl.jakubmuczyn.logic;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import pl.jakubmuczyn.TaskConfigurationProperties;
+import org.springframework.web.context.annotation.RequestScope;
 import pl.jakubmuczyn.model.TaskGroup;
 import pl.jakubmuczyn.model.TaskGroupRepository;
 import pl.jakubmuczyn.model.TaskRepository;
@@ -14,12 +14,13 @@ import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
+@RequestScope // w obrębie jednego żądania mamy unikalną instancję serwisu (@Scope - adnotacja mówiąca jak ten obiekt powinien być wstrzykiwany)
 public class TaskGroupService {
     
     private TaskGroupRepository repository;
     private TaskRepository taskRepository;
     
-    public GroupReadModel createGroup(GroupWriteModel source) {
+    public GroupReadModel createGroup(final GroupWriteModel source) {
         TaskGroup result = repository.save(source.toGroup());
         return new GroupReadModel(result);
     }
