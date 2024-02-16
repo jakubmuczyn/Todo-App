49ede85decca1c0617b67b5fc8d49df815dd3586
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-16 20:39:50 +0100

    Poprawka kosmetyczna

--- src/main/java/pl/jakubmuczyn/controller/TaskController.java
+++ src/main/java/pl/jakubmuczyn/controller/TaskController.java
@@ -72,15 +72,4 @@ class TaskController {
                 .ifPresent(task -> task.setDone(!task.isDone()));
         return ResponseEntity.noContent().build();
     }
-    
-    @Transactional
-    @PatchMapping("/tasks/{id}")
-    public ResponseEntity<?> toggleTask(@PathVariable int id) {
-        if (!repository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        repository.findById(id)
-                .ifPresent(task -> task.setDone(!task.isDone()));
-        return ResponseEntity.noContent().build();
-    }
 }
