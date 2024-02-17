8000e79145800eecff15566455d6d73518fbf0fb
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-17 11:04:37 +0100

    JUnit

--- src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
+++ src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
@@ -1,5 +1,6 @@
 package pl.jakubmuczyn.logic;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProjectServiceTest {
     
     @Test
-    void createGroup() {
+    @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists.")
+    void createGroup_noMultipleGroupsConfig_and_undoneGroupExtsts_throwsIllegalStateException() {
+        // given
+        var mockGroupRepository;
+        
+        // when
+        
+        // then
+        
     }
 }
