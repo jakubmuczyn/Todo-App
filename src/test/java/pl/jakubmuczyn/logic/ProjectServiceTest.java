27214adf8d9810666edfe1e68a79a936ae3c453e
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-17 13:55:34 +0100

    Mock in mock

--- src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
+++ src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
@@ -3,9 +3,7 @@ package pl.jakubmuczyn.logic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.jakubmuczyn.TaskConfigurationProperties;
-import pl.jakubmuczyn.model.ProjectRepository;
-import pl.jakubmuczyn.model.TaskGroup;
-import pl.jakubmuczyn.model.TaskGroupRepository;
+import pl.jakubmuczyn.model.*;
 import pl.jakubmuczyn.model.projection.GroupReadModel;
 
 import java.time.LocalDate;
@@ -22,12 +20,11 @@ class ProjectServiceTest {
     
     @Test
     @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists.")
-    void createGroup_noMultipleGroupsConfig_and_undoneGroupExtsts_throwsIllegalStateException() {
+    void createGroup_noMultipleGroupsConfig_and_undoneGroupExists_throwsIllegalStateException() {
         // given
-        TaskGroupRepository mockGroupRepository = groupRepositoryReturning(true);
-        
-        TaskConfigurationProperties mockConfig = configurationReturning(false);
-        
+        TaskGroupRepository mockGroupRepository = taskGroupRepositoryReturning(true);
+        // and
+        TaskConfigurationProperties mockConfig = taskConfigurationPropertiesReturning(false);
         // system under test
         var projectServiceToTest = new ProjectService(null, mockGroupRepository, mockConfig);
         
@@ -46,9 +43,8 @@ class ProjectServiceTest {
         // given
         var mockRepository = mock(ProjectRepository.class);
         when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
-        
-        TaskConfigurationProperties mockConfig = configurationReturning(true);
-        
+        // and
+        TaskConfigurationProperties mockConfig = taskConfigurationPropertiesReturning(true);
         // system under test
         var projectServiceToTest = new ProjectService(mockRepository, null, mockConfig);
         
@@ -63,15 +59,14 @@ class ProjectServiceTest {
     
     @Test
     @DisplayName("should throw IllegalArgumentException when configured to allow just 1 group and no groups and projects for given id.")
-    void createGroup_noMultipleGroupsConfig_and_noUndoneGroupExtsts_noProjects_throwsIllegalArgumentException() {
+    void createGroup_noMultipleGroupsConfig_and_noUndoneGroupExists_noProjects_throwsIllegalArgumentException() {
         // given
         var mockRepository = mock(ProjectRepository.class);
         when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
-        
-        TaskGroupRepository mockGroupRepository = groupRepositoryReturning(false);
-        
-        TaskConfigurationProperties mockConfig = configurationReturning(true);
-        
+        // and
+        TaskGroupRepository mockGroupRepository = taskGroupRepositoryReturning(false);
+        // and
+        TaskConfigurationProperties mockConfig = taskConfigurationPropertiesReturning(true);
         // system under test
         var projectServiceToTest = new ProjectService(mockRepository, mockGroupRepository, mockConfig);
         
@@ -89,15 +84,16 @@ class ProjectServiceTest {
     void createGroup_configurationOk_existingProject_createsAndSavesGroup() {
         // given
         var today = LocalDate.now().atStartOfDay();
-        
+        // and
+        var project = projectWith("project description", Set.of(-1, -2));
         var mockRepository = mock(ProjectRepository.class);
-        when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
-        
+        when(mockRepository.findById(anyInt()))
+                .thenReturn(Optional.of(project));
+        // and
         InMemoryGroupRepository inMemoryGroupRepo = inMemoryGroupRepository();
         int countBeforeCall = inMemoryGroupRepo.count();
-        
-        TaskConfigurationProperties mockConfig = configurationReturning(true);
-        
+        // and
+        TaskConfigurationProperties mockConfig = taskConfigurationPropertiesReturning(true);
         // system under test
         var projectServiceToTest = new ProjectService(mockRepository, inMemoryGroupRepo, mockConfig);
         
@@ -105,18 +101,33 @@ class ProjectServiceTest {
         GroupReadModel result = projectServiceToTest.createGroup(today, 1);
         
         // then
-        assertThat(countBeforeCall + 1)
-                .isEqualTo(inMemoryGroupRepo.count());
-        
+        assertThat(result.getDescription()).isEqualTo("project description");
+        assertThat(result.getDeadline()).isEqualTo(today.minusDays(1));
+        assertThat(result.getTasks()).allMatch(task -> task.getDescription().equals("test description"));
+        assertThat(countBeforeCall + 1).isEqualTo(inMemoryGroupRepo.count());
     }
     
-    private static TaskGroupRepository groupRepositoryReturning(final boolean value) {
+    private Project projectWith(String projectDescription, Set<Integer> daysToDeadline) {
+        Set<ProjectStep> steps = daysToDeadline.stream()
+                .map(days -> {
+                    var step = mock(ProjectStep.class);
+                    when(step.getDescription()).thenReturn("test description");
+                    when(step.getDaysToDeadline()).thenReturn(days);
+                    return step;
+                }).collect(Collectors.toSet());
+        var result = mock(Project.class);
+        when(result.getDescription()).thenReturn(projectDescription);
+        when(result.getSteps()).thenReturn(steps);
+        return result;
+    }
+    
+    private static TaskGroupRepository taskGroupRepositoryReturning(final boolean value) {
         var mockGroupRepository = mock(TaskGroupRepository.class);
         when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(value);
         return mockGroupRepository;
     }
     
-    private static TaskConfigurationProperties configurationReturning(final boolean value) {
+    private static TaskConfigurationProperties taskConfigurationPropertiesReturning(final boolean value) {
         var mockTemplate = mock(TaskConfigurationProperties.Template.class);
         when(mockTemplate.isAllowMultipleTasks()).thenReturn(value); // pierwszy warunek z IFa
         
@@ -152,7 +163,9 @@ class ProjectServiceTest {
         public TaskGroup save(final TaskGroup entity) {
             if (entity.getId() == 0) {
                 try {
-                    TaskGroup.class.getDeclaredField("id").set(entity, ++index); // refleksja
+                    var field = TaskGroup.class.getDeclaredField("id"); // refleksja
+                    field.setAccessible(true);
+                    field.set(entity, ++index);
                 } catch (NoSuchFieldException | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
