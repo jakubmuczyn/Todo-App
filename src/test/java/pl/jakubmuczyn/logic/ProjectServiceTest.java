ec7835de971087d979f12b16eda95a4369855465
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-17 11:17:08 +0100

    Mockito

--- src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
+++ src/test/java/pl/jakubmuczyn/logic/ProjectServiceTest.java
@@ -2,8 +2,15 @@ package pl.jakubmuczyn.logic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pl.jakubmuczyn.TaskConfigurationProperties;
+import pl.jakubmuczyn.model.TaskGroupRepository;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProjectServiceTest {
     
@@ -11,11 +18,23 @@ class ProjectServiceTest {
     @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists.")
     void createGroup_noMultipleGroupsConfig_and_undoneGroupExtsts_throwsIllegalStateException() {
         // given
-        var mockGroupRepository;
+        var mockGroupRepository = mock(TaskGroupRepository.class);
+        when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(true);
+        
+        var mockTemplate = mock(TaskConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleTasks()).thenReturn(false);
+        
+        var mockConfig = mock(TaskConfigurationProperties.class);
+        when(mockConfig.getTemplate()).thenReturn(mockTemplate);
+        
+        // system under test
+        var toTest = new ProjectService(null, mockGroupRepository, mockConfig);
         
         // when
+        toTest.createGroup(LocalDateTime.now(), 0);
         
         // then
+        
         
     }
 }
