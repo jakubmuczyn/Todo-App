392484f5bce8402214880ddf807fea57f2b67376
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-18 14:04:02 +0100

    @RequestMapping in InfoController

--- src/main/java/pl/jakubmuczyn/controller/InfoController.java
+++ src/main/java/pl/jakubmuczyn/controller/InfoController.java
@@ -3,22 +3,24 @@ package pl.jakubmuczyn.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.jakubmuczyn.TaskConfigurationProperties;
 
 @AllArgsConstructor
+@RequestMapping("/info")
 @RestController
 public class InfoController {
     
     private DataSourceProperties dataSource;
     private TaskConfigurationProperties myProp;
     
-    @GetMapping("/info/url")
+    @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
     
-    @GetMapping("/info/prop")
+    @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
     }
