e0197174c15fbcf8902c52e8c71a952f8834934e
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-18 14:20:56 +0100

    doFilter

--- src/main/java/pl/jakubmuczyn/controller/LoggerFilter.java
+++ src/main/java/pl/jakubmuczyn/controller/LoggerFilter.java
@@ -1,15 +1,13 @@
 package pl.jakubmuczyn.controller;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Order(Ordered.HIGHEST_PRECEDENCE) // HIGHEST_PRECEDENCE jest domyślną wartością
 @Component
 public class LoggerFilter implements Filter {
     
@@ -17,5 +15,10 @@ public class LoggerFilter implements Filter {
     
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            var httpRequest = (HttpServletRequest) request;
+            logger.info("[doFilter] " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+        }
+        chain.doFilter(request, response);
     }
 }
