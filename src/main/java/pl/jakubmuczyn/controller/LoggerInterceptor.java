756f60cb5ac9b3586baabe58f182cff60f5fb1ab
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-18 14:32:50 +0100

    Interceptor

---
+++ src/main/java/pl/jakubmuczyn/controller/LoggerInterceptor.java
@@ -0,0 +1,17 @@
+package pl.jakubmuczyn.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+class LoggerInterceptor implements HandlerInterceptor {
+    
+    private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        logger.info("[preHandle] " + request.getMethod() + " " + request.getRequestURI());
+        return true;
+    }
+}
