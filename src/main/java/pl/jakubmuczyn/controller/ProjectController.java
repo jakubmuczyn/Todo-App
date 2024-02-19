8e4de2a8f4f6a62322e086640643edaef21f97df
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-19 13:47:34 +0100

    Model from MVC (th:object)

--- src/main/java/pl/jakubmuczyn/controller/ProjectController.java
+++ src/main/java/pl/jakubmuczyn/controller/ProjectController.java
@@ -1,15 +1,18 @@
 package pl.jakubmuczyn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.jakubmuczyn.model.projection.ProjectWriteModel;
 
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
     
     @GetMapping
-    String showProjects() {
+    String showProjects(Model model) {
+        model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
 }
