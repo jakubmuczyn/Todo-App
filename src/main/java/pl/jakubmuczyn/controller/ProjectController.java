7ff87de137c261167a94492ba0335e2a265928eb
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-19 14:18:56 +0100

    @ModelAttribute

--- src/main/java/pl/jakubmuczyn/controller/ProjectController.java
+++ src/main/java/pl/jakubmuczyn/controller/ProjectController.java
@@ -3,7 +3,10 @@ package pl.jakubmuczyn.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.jakubmuczyn.model.ProjectStep;
 import pl.jakubmuczyn.model.projection.ProjectWriteModel;
 
 @Controller
@@ -13,6 +16,12 @@ class ProjectController {
     @GetMapping
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+    
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel projectWriteModel) {
+        projectWriteModel.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
