047183a5b6afcfe5e54cc5fae1a3294ad9e6fd6e
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-20 13:02:57 +0100

    Method handling @Around advice

--- src/main/java/pl/jakubmuczyn/aspect/LogicAspect.java
+++ src/main/java/pl/jakubmuczyn/aspect/LogicAspect.java
@@ -1,9 +1,22 @@
 package pl.jakubmuczyn.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+/**
+ * Moglibyśmy całą obsługę logiki biznesowej przenieść do aspektów, lecz nie jest
+ * to zalecane, chociażby dlatego, że zwracamy Objects, które trzeba byłoby
+ * rzutować na bardziej specyficzne klasy, czy przez inne niewygodne tematy.
+ * */
 
 @Aspect // faktyczna obsługa aspektów
 @Component // zarejestrowanie w Springu tej klasy
 class LogicAspect {
+    
+    @Around()
+    Object aroungCreateGroupByProject(ProceedingJoinPoint joinPoint) {
+        return joinPoint.proceed();
+    }
 }
