de7bfb80e76b120b6a46402d5402414530840244
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-20 19:11:11 +0100

    Events log

--- src/main/java/pl/jakubmuczyn/reports/ChangedTaskEventListener.java
+++ src/main/java/pl/jakubmuczyn/reports/ChangedTaskEventListener.java
@@ -5,21 +5,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import pl.jakubmuczyn.model.event.TaskDone;
+import pl.jakubmuczyn.model.event.TaskEvent;
 import pl.jakubmuczyn.model.event.TaskUndone;
 
 @Service // service a nie component, bo jest to rzecz bardziej związana z procesowaniem/usługowością
 class ChangedTaskEventListener {
     
     private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
+    private final PersistedTaskEventRepository persistedTaskEventRepository;
+    
+    ChangedTaskEventListener(final PersistedTaskEventRepository persistedTaskEventRepository) {
+        this.persistedTaskEventRepository = persistedTaskEventRepository;
+    }
     
     @EventListener
     public void on(TaskDone event) {
-        logger.info("Got " + event);
+        onChanged(event);
     }
     
     @EventListener
     public void on(TaskUndone event) {
+        onChanged(event);
+    }
+    
+    private void onChanged(final TaskEvent event) {
         logger.info("Got " + event);
+        persistedTaskEventRepository.save(new PersistedTaskEvent(event));
     }
     
 }
