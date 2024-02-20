c69abc5e19f584c6a6643c0bd52313fce68e8dbf
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-20 13:22:36 +0100

    @Around advice - implementation of metrics

--- src/main/java/pl/jakubmuczyn/aspect/LogicAspect.java
+++ src/main/java/pl/jakubmuczyn/aspect/LogicAspect.java
@@ -1,5 +1,7 @@
 package pl.jakubmuczyn.aspect;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,14 +11,33 @@ import org.springframework.stereotype.Component;
  * Moglibyśmy całą obsługę logiki biznesowej przenieść do aspektów, lecz nie jest
  * to zalecane, chociażby dlatego, że zwracamy Objects, które trzeba byłoby
  * rzutować na bardziej specyficzne klasy, czy przez inne niewygodne tematy.
- * */
+ */
 
 @Aspect // faktyczna obsługa aspektów
 @Component // zarejestrowanie w Springu tej klasy
 class LogicAspect {
     
-    @Around("execution(* pl.jakubmuczyn.logic.ProjectService.createGroup(..))") // * - dowolny typ zwracany, .. - jakieś parametry
+    private final Timer createGroupByProjectTimer;
+    
+    public LogicAspect(final MeterRegistry meterRegistry) { // podczas tworzenia tej klasy Sprign wstrzyknie rejestr metryk. MeterRegistry pozwala też tworzyć czasomierze
+        createGroupByProjectTimer = meterRegistry.timer("logic.project.create.group");
+    }
+    
+    // @Around - aspekt
+    // * - dowolny typ zwracany
+    // .. - jakieś parametry
+    @Around("execution(* pl.jakubmuczyn.logic.ProjectService.createGroup(..))")
     Object aroungCreateGroupByProject(ProceedingJoinPoint joinPoint) {
-        return joinPoint.proceed();
+        return createGroupByProjectTimer.record(() -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable e) {
+                if (e instanceof RuntimeException) {
+                    throw (RuntimeException) e;
+                } else {
+                    throw new RuntimeException(e); // ręczne owrapowanie wyjątku w RuntimeException
+                }
+            }
+        });
     }
 }
