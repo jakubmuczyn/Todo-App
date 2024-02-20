7fb8d2d7e3905136ec1ae59addff26eec5d573e9
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-20 12:55:05 +0100

    @Aspect

---
+++ src/main/java/pl/jakubmuczyn/aspect/LogicAspect.java
@@ -0,0 +1,9 @@
+package pl.jakubmuczyn.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect // faktyczna obsługa aspektów
+@Component // zarejestrowanie w Springu tej klasy
+class LogicAspect {
+}
